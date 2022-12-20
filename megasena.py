866f76657a0e74212e51f07d4c9bc6a0a561f8b5
Tiago Silva <tiago.techsuport@gmail.com>
2022-12-20 14:59:06 -0300

    Adciona barra de progresso e otimiza utilização de bibliotecas

--- megasena.py
+++ megasena.py
@@ -1,20 +1,22 @@
-import requests
+from requests import get
 import json
-import pandas as pd
+from pandas import DataFrame as pd
 import urllib3
-import pandas as pd
-import random
+from random import choices
 urllib3.disable_warnings()
 from pprint import pprint
 import time
+from tqdm import tqdm
 
 lmega = 'https://servicebus2.caixa.gov.br/portaldeloterias/api/megasena/'
 headers =  {"Content-Type":"application/json"}
-resmega = requests.get(lmega, headers, verify=False)
+resmega = get(lmega, headers, verify=False)
 resmega = resmega.json()
 conc_final = int(resmega['numeroConcursoProximo'])
 
-print("Ultimo concurso  = ", conc_final)
+print("Proximo concurso  = ", conc_final)
+
+print(100*' '  )
 '''
 Variaveis 
 '''
@@ -25,21 +27,26 @@ base = []
 repetidos = []
 palpites = []
 
-qtdAnalise = int(input('Informe a quantidade de Concurso a Analisar'))
-qtdPalpite = int(input('Informe a quantidade de Palpites a Gerar'))
+qtdAnalise = int(input('Informe a quantidade de Concurso a Analisar: '))
+qtdPalpite = int(input('Informe a quantidade de Palpites a Gerar: '))
 
-for i in range(conc_final-qtdAnalise, conc_final):
+print(100*' '  )
+
+print("Analisando Concurso")
+
+for i in tqdm(range(conc_final-qtdAnalise, conc_final)):
     
     time.sleep(1)
 
     link = f'https://servicebus2.caixa.gov.br/portaldeloterias/api/megasena/{i}'
     headers =  {"Content-Type":"application/json"}
-    res = requests.get(link, headers, verify=False)
+    res = get(link, headers, verify=False)
     #, "Authorization": f"Bearer {token}"
     resposta = res.json()
     sorteados.append(sorted(resposta['dezenasSorteadasOrdemSorteio']))
     sorteados_ordenados = sorted(sorteados)
     concurso.append(i)
+
 
 for x in range(1,len(sorteados)+1):
     index.append(x)
@@ -52,16 +59,21 @@ for i in sorteados:
         else:
             base.append(x)
 
+base = sorted(base)
 
+print(100*' '  )
 
-for x in range(qtdPalpite):
-    a = random.choices(base, k=1)
+print("Gerando Palpite")
+
+for x in tqdm(range(qtdPalpite)):
+    time.sleep(0.5)
+    a = choices(base, k=1)
     palpite = []
     contador = 0
     if a not in palpite:
         palpite.append(a)
         while (len(palpite) < 6):
-            a = random.choices(base, k=1)
+            a = choices(base, k=1)
             if a not in palpite:
                 palpite.append(a)
             contador +=1
@@ -72,15 +84,29 @@ df = {
         'Numeros Sorteados':sorteados,
     }   
 
-df = pd.DataFrame(df,index=index)
+df = pd(df,index=index)
+
+print(100*' '  )
+
+print(10*'*','TOTAL DE NUMERO A SER ANALISADOS',10*'*'  )
+print(len(base))
+
+print(100*' '  )
 
 print(10*'*','BASE UTILIZADA',10*'*'  )
+
 print(base)
+
+print(100*' '  )
 
 print(10*'*','CONCURSO ANALISADOS',10*'*'  )
 print(df)
 
+print(100*' '  )
+
 print(10*'*','PALPITES GERADOS ',10*'*'  )
+
+print(100*' '  )
 
 for y in palpites:
     print(y)
